9a560d4b853ccf092ff9b921b7ddf05d516a67cb
fwhaslam <fwhaslam@gmail.com>
2021-11-30 15:45:13 -0800

    Adding ConsoleMocker to test against Console.Out.

---
+++ Verbose/Utility/ConsoleMocker.cs
@@ -0,0 +1,88 @@
+﻿//
+//	Copyright 2021 Frederick William Haslam born 1962 in the USA
+//
+
+namespace Verbose.Utility {
+
+	using System;
+	using System.Globalization;
+	using System.IO;
+	using System.Text;
+
+	/// <summary>
+	/// TextWriter that can be used to replace Console.Out for test purposes.
+	/// </summary>
+	public class ConsoleMocker : TextWriter {
+
+		// Record all writes to TextWriter
+		internal StringBuilder Result = new StringBuilder();
+		internal TextWriter OutWriter;
+
+		public override Encoding Encoding => Encoding.Default;
+
+		internal ConsoleMocker( TextWriter outWriter) : base(CultureInfo.InvariantCulture) { 
+			this.OutWriter = outWriter;
+		}
+
+//======================================================================================================================
+//		TextWriter methods
+
+		public override void Write(char[] buffer, int index, int count) {
+			for (int i = 0; i < count; i++) {
+				Result.Append( buffer[index + i] );
+			}
+		}
+
+		public override void Write(string value) {
+			Result.Append( value );
+		}
+
+		public override void WriteLine() {
+			Result.Append( Environment.NewLine );
+		}
+
+		public override void WriteLine(string value) {
+			Result.Append( value ).Append( Environment.NewLine);
+		}
+
+		public override void WriteLine(object value) {
+			Result.Append( value ).Append( Environment.NewLine);
+		}
+
+//======================================================================================================================
+//		Testing methods
+
+		/// <summary>
+		/// Replaces the Console.Out, and stores it for later restoration.
+		/// </summary>
+		/// <returns></returns>
+		static public ConsoleMocker MockConsoleOut() {
+			var writer = new ConsoleMocker( Console.Out );
+			Console.SetOut( writer );
+			return writer;
+		}
+
+		/// <summary>
+		/// This should be called in a 'finally' block after a try block which mocks the console.
+		/// </summary>
+		public void RestoreConsoleOut() {
+			Console.SetOut( OutWriter );
+		}
+
+		/// <summary>
+		/// System specific result, includes carriage returns (\r) for windows.
+		/// </summary>
+		/// <returns></returns>
+		public string GetSpecificResult() {
+			return Result.ToString();
+		}
+
+		/// <summary>
+		/// System agnostic result, remove all carriage returns (\r).  EndOfLine is now simply line feed (\n).
+		/// </summary>
+		/// <returns></returns>
+		public string GetResult() {
+			return Result.Replace("\r","").ToString();
+		}
+	}
+}
---
+++ VerboseTests/Utility/ConsoleMockerTest.cs
@@ -0,0 +1,152 @@
+﻿//
+//	Copyright 2021 Frederick William Haslam born 1962 in the USA
+//
+
+
+namespace Verbose.Utility {
+
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+		using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+	[TestClass]
+	public class ConsoleMockerTest {
+
+		
+		[TestMethod]
+		public void Constructor() { 
+
+			var writer = Console.Out;
+
+			// invocation
+			var result = new ConsoleMocker( writer );
+
+			// assertions
+			AreSame( writer, result.OutWriter);
+			AreEqual( 0, result.Result.Length );
+		}
+		
+		[TestMethod]
+		public void Write_buffer() { 
+
+			var console = new ConsoleMocker( null );
+
+			// invocation
+			console.Write( "Hello".ToCharArray(), 1, 3 );
+
+			// assertions
+			AreEqual( "ell", console.GetResult() );
+		}
+		
+		[TestMethod]
+		public void Write_string() { 
+
+			var console = new ConsoleMocker( null );
+
+			// invocation
+			console.Write( "Hello" );
+
+			// assertions
+			AreEqual( "Hello", console.GetResult() );
+		}
+		
+		[TestMethod]
+		public void WriteLine_string() { 
+
+			var console = new ConsoleMocker( null );
+
+			// invocation
+			console.WriteLine( "Hello" );
+
+			// assertions
+			AreEqual( "Hello\n", console.GetResult() );
+		}
+		
+		[TestMethod]
+		public void WriteLine_Object() { 
+
+			var console = new ConsoleMocker( null );
+
+			// invocation
+			console.WriteLine( 123 );
+
+			// assertions
+			AreEqual( "123\n", console.GetResult() );
+		}
+		
+		[TestMethod]
+		public void MockConsoleOut() { 
+
+			var startingConsole = Console.Out;
+
+			// invocation
+			var result = ConsoleMocker.MockConsoleOut();
+
+			// assertions
+			try {
+				AreSame( startingConsole, result.OutWriter );
+
+				// our writer is wrapped in a SyncWriter, so cannot directly assert
+				//AreSame( result, Console.Out );
+
+				// check that Console.Out records to our writer
+				Console.Out.Write("Hello");
+				AreEqual( "Hello", result.GetResult() );
+			}
+			// cleanup
+			finally {
+				result.RestoreConsoleOut();
+				AreSame( startingConsole, Console.Out );
+			}
+		}
+		
+		[TestMethod]
+		public void RestoreConsoleOut() { 
+
+			var startingConsole = Console.Out;
+			var console = ConsoleMocker.MockConsoleOut();
+
+			// invocation
+			console.RestoreConsoleOut();
+
+			// assertions
+			AreSame( startingConsole, Console.Out );
+		}
+		
+		[TestMethod]
+		public void GetSpecificResult() { 
+
+			var console = new ConsoleMocker( null );
+			console.WriteLine( 123 );
+			var expect = "123"+Environment.NewLine;
+
+			// invocation
+			var result = console.GetSpecificResult();
+
+			// assertions
+			AreEqual( expect, result );
+		}
+		
+		[TestMethod]
+		public void GetResult() { 
+
+			var console = new ConsoleMocker( null );
+			console.WriteLine( 123 );
+			var expect = "123\n";
+
+			// invocation
+			var result = console.GetResult();
+
+			// assertions
+			AreEqual( expect, result );
+		}
+		
+	}
+}
