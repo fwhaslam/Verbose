feb7c1a11e4620f74820baf6ffa66f6d48740a84
fwhaslam <fwhaslam@gmail.com>
2022-12-05 16:53:29 -0800

    Fix for multiple slash issue discovered in DRM/YamlTools.

---
+++ VerboseTests/Utility/VerboseAsserts_NewBugsTest.cs
@@ -0,0 +1,54 @@
+﻿//
+//	Copyright 2021 Frederick William Haslam born 1962 in the USA
+//
+
+namespace VerboseTests.Utility {
+
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	using Verbose.Utility;
+
+	using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+	[TestClass]
+	public class VerboseAsserts_NewBugsTest {
+
+		[TestMethod]
+		public void TacticalDungeon_YamlTools_Bug_minimal() {
+
+			// the triple slash is reduced to a double slash in IssueToDisplay()
+			string source = "StrField: (\"he\\\"llo\",False)";
+			string[] actual = source.Split('\n');
+
+			// invocation
+			var result = VerboseAsserts.IssueToDisplay( null, null, actual );
+
+			// assertions = remains 'triple' slash when displayed
+System.Console.WriteLine("RESULT=[[[\n"+result+"\n]]]");
+
+			AreEqual( "[[\"StrField: (\\\"he\\\\\\\"llo\\\",False)\"]]\n", result );
+		}
+
+		[TestMethod]
+		public void TacticalDungeon_YamlTools_Bug() {
+
+			// the triple slash is reduced to a double slash in IssueToDisplay()
+			string source = 
+				"CharField: ('x')\n"+
+				"StrField: (\"he\\\"llo\",False)\n"+
+				"NumField: (123,10.5)\n";
+			string[] actual = source.Split('\n');
+
+			// invocation
+			var result = VerboseAsserts.IssueToDisplay( null, null, actual );
+
+			// assertions = remains 'triple' slash when displayed
+//System.Console.WriteLine("RESULT=[[[\n"+result+"\n]]]");
+			AreEqual( "[[\"CharField: ('x')\\n\"+\n"+
+				"\t\t\"StrField: (\\\"he\\\\\\\"llo\\\",False)\\n\"+\n"+
+				"\t\t\"NumField: (123,10.5)\\n\"+\n"+
+				"\t\t\"\"]]\n", result );
+		}
+
+	}
+}
